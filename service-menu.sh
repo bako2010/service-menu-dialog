47784e8df64ad60c398e951ff9f595ee024fcf86
Ba Ko <46249901+bako2010@users.noreply.github.com>
2026-04-24 11:49:48 +0200

    Update service-menu.sh

--- service-menu.sh
+++ service-menu.sh
@@ -62,7 +62,7 @@ while true; do
     header=$(printf " %-4s | %-35s | %-20s | %-12s | %-18s | %-12s | %-7s" "NR" "SERVICE-NAME" "PATH" "OWNER" "STATE" "ACTIVE" "RAM")
     divider=$(printf '%.s-' {1..165})
     
-    legend="written by -- KingKon --
+    legend="written by -- KingKon --    
 STATE:      enabled (Auto), disabled (Manuell), static (Abhaeng.), masked (Gesperrt)
 ACTIVE:     active (laeuft), inactive (aus), failed (Fehler)
 OWNER/RAM:  Besitzer des Dienstes | Aktueller Arbeitsspeicherverbrauch in MB"
